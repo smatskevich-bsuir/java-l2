06b1532128390a16318d7473f57b290d6880973d
Stanislau Matskevich <st.matskevich.work@gmail.com>
2020-03-28 01:56:52 +0300

    All in one

---
+++ src/fleet/Ship.java
@@ -0,0 +1,16 @@
+package fleet;
+
+public class Ship {
+	private String name;
+	private int displacement;
+	
+	public Ship(String name, int displacement)
+	{
+		this.name = name;
+		this.displacement = displacement;
+	}
+	
+	public void swim() {
+		//move instead of swim
+	}
+}
